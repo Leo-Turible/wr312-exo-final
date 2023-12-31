681ef783a52f134d3b82045a274e14e022915044
Leo-Turible <turibleleo@gmail.com>
2023-12-31 15:30:52 +0100

    2 for scss added

--- assets/js/menuBurger.js
+++ assets/js/menuBurger.js
@@ -4,13 +4,10 @@ let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
-        // menuBtn.classList.add('mon-menu1');
-        //monMenu.classList.replace('mon-menu','mon-menu1');
         monMenu.classList.add('mobile-menu');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
-        // monMenu.classList.replace('mon-menu','mon-menu1');
         monMenu.classList.remove('mobile-menu');
         menuOpen = false;
     }
